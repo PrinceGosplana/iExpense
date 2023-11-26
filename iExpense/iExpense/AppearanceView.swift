1f1ee0d886c6784462375ce13bff058756dcbf2e
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-26 06:51:50 +0200

    add new view for show hide view

---
+++ iExpense/iExpense/AppearanceView.swift
@@ -0,0 +1,18 @@
+//
+//  AppearanceView.swift
+//  iExpense
+//
+//  Created by OLEKSANDR ISAIEV on 26.11.2023.
+//
+
+import SwiftUI
+
+struct AppearanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppearanceView()
+}
