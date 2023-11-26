5771fe66f6829fe640642ce3b36f1e6293cd795e
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-26 06:34:53 +0200

    add new view with state binding

---
+++ iExpense/iExpense/ClassWithStateView.swift
@@ -0,0 +1,28 @@
+//
+//  ClassWithStateView.swift
+//  iExpense
+//
+//  Created by OLEKSANDR ISAIEV on 26.11.2023.
+//
+
+import SwiftUI
+
+struct User {
+    var firstName = "Lama"
+}
+
+struct ClassWithStateView: View {
+    @State private var user = User()
+    
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName)")
+            TextField("Name", text: $user.firstName)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ClassWithStateView()
+}
