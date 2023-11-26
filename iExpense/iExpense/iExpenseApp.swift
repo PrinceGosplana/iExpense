9b5bc99406bd6eee15a3820cee68c310c1c0f1a7
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-26 06:00:55 +0200

    init new project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by OLEKSANDR ISAIEV on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
