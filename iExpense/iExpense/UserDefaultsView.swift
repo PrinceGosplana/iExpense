7fef3ddab43658db00802f408d4ea844bfaa43db
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-28 06:21:25 +0200

    add new view for user defaults reason

---
+++ iExpense/iExpense/UserDefaultsView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDefaultsView.swift
+//  iExpense
+//
+//  Created by OLEKSANDR ISAIEV on 28.11.2023.
+//
+
+import SwiftUI
+
+struct UserDefaultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDefaultsView()
+}
