10a28d3427bbe7e6e357f1bdc6d8882d24899805
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-28 06:34:19 +0200

    replace with faster appstorage

--- iExpense/iExpense/UserDefaultsView.swift
+++ iExpense/iExpense/UserDefaultsView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct UserDefaultsView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "tap")
+    @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
         Button("Tap count: \(tapCount)") {
             tapCount += 1
-            UserDefaults.standard.set(tapCount, forKey: "tap")
         }
     }
 }
