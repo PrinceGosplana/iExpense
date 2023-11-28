d9d9ddd460c1b31a53f604f2b67fbd3583ff4674
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-28 06:23:58 +0200

    add userdefaults example

--- iExpense/iExpense/UserDefaultsView.swift
+++ iExpense/iExpense/UserDefaultsView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct UserDefaultsView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
+            UserDefaults.standard.set(tapCount, forKey: "tap")
+        }
     }
 }
 
