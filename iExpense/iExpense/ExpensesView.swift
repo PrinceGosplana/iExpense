895771aa1f98b0d86385fef78e97a358109636e7
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-02 19:08:38 +0200

    append addview for create new expense

--- iExpense/iExpense/ExpensesView.swift
+++ iExpense/iExpense/ExpensesView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ExpensesView: View {
     @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationStack {
@@ -21,9 +22,11 @@ struct ExpensesView: View {
             .navigationTitle("iExpense")
             .toolbar {
                 Button("Add expense", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
