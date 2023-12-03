dba6632d31f7824d69d010114ff0cf5479c59468
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-03 06:16:33 +0200

    link add screen with edit screen

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -30,6 +30,14 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name,
+                                           type: type,
+                                           amount: amount)
+                    expenses.items.append(item)
+                }
+            }
         }
         
     }
