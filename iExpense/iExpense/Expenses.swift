8b76167dd68b04bea6f76cb74d47e033d8d87422
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-03 06:48:55 +0200

    making changes permanent with userdefaults

--- iExpense/iExpense/Expenses.swift
+++ iExpense/iExpense/Expenses.swift
@@ -9,5 +9,23 @@ import SwiftUI
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        guard let savedItems = UserDefaults.standard.data(forKey: "Items") else {
+            items = []
+            return
+        }
+        guard let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) else {
+            items = []
+            return
+        }
+        items = decodedItems
+    }
 }
